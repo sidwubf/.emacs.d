60e60ce5ecd15a384bd319bd91701912f9544a24
Jenson Wu <wubofengwbf@gmail.com>
2012-09-19 10:40:44 +0800

    add moccur

--- init-func.el
+++ init-func.el
@@ -8,3 +8,5 @@
   "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
   (dolist (pattern patterns)
     (add-to-list 'auto-mode-alist (cons pattern mode))))
+
+(require 'color-moccur)
