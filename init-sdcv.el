6a1186517e28c3b5a17dbb538c449a69860021e3
jensonwu <bofengwu@anjuke.com>
2013-02-22 16:33:45 +0800

    fix

--- init-sdcv.el
+++ init-sdcv.el
@@ -1,22 +1,22 @@
 ;(require 'showtip)
 ;(require 'sdcv)
 
-(defun kid-star-dict ()  
-  (interactive)  
-  (let ((begin (point-min))  
-        (end (point-max)))  
-    (if mark-active  
-        (setq begin (region-beginning)  
-              end (region-end))  
-      (save-excursion  
-        (backward-word)  
-        (mark-word)  
-        (setq begin (region-beginning)  
-              end (region-end))))  
-    ;; 有时候 stardict 会很慢，所以在回显区显示一点东西  
-    ;; 以免觉得 Emacs 在干什么其他奇怪的事情。  
-    (message "searching for %s ..." (buffer-substring begin end))  
-    (tooltip-show   
-     (shell-command-to-string   
-      (concat "sdcv -n "   
-              (buffer-substring begin end))))))  
+(defun kid-star-dict ()
+  (interactive)
+  (let ((begin (point-min))
+        (end (point-max)))
+    (if mark-active
+        (setq begin (region-beginning)
+              end (region-end))
+      (save-excursion
+        (backward-word)
+        (mark-word)
+        (setq begin (region-beginning)
+              end (region-end))))
+    ;; 有时候 stardict 会很慢，所以在回显区显示一点东西
+    ;; 以免觉得 Emacs 在干什么其他奇怪的事情。
+    (message "searching for %s ..." (buffer-substring begin end))
+    (tooltip-show
+     (shell-command-to-string
+      (concat "sdcv -n "
+              (buffer-substring begin end))))))
