8206b8ab25d5dad6c57fb1d374bbd38a3f0fd031
jensonwu <bofengwu@anjuke.com>
2013-02-26 09:25:53 +0800

    delete doc tab

--- init-doc.el
+++ init-doc.el
@@ -2,7 +2,6 @@
 (setq php-doc-directory "/home/flash/manual/php-manual-en")
 (add-hook 'php-mode-hook
           (lambda ()
-            (local-set-key "\t" 'php-doc-complete-function)
             (local-set-key (kbd "\C-c h") 'php-doc)
             (set (make-local-variable 'eldoc-documentation-function)
                  'php-doc-eldoc-function)
