5cfc69f7c7e83eea6846102ede69b524ae5161bb
吴波峰 <wubofengwbf@gmail.com>
2013-11-21 20:39:57 +0800

    Create init-erlang.el

---
+++ init-erlang.el
@@ -0,0 +1,4 @@
+(setq load-path (cons"/usr/local/Cellar/erlang/R16B02/lib/erlang/lib/tools-2.6.12/emacs" load-path))
+(setq erlang-root-dir "/usr/local/Cellar/erlang/R16B02/lib/erlang/lib")
+(setq exec-path (cons "/usr/local/Cellar/erlang/R16B02/lib/erlang/bin" exec-path))
+(require 'erlang-start)
